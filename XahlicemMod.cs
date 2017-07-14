5ccaa973fa29be104e82246245c9ed50005eed5d
Xahlicem <xahlicem@gmail.com>
2017-07-13 21:43:52 -0700

    Removed unused imports.

--- XahlicemMod.cs
+++ XahlicemMod.cs
@@ -1,16 +1,4 @@
-using Terraria;
-using Terraria.Graphics.Effects;
-using Terraria.Graphics.Shaders;
-using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
-using System.IO;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria.UI;
-using Terraria.DataStructures;
-using Terraria.GameContent.UI;
 
 namespace XahlicemMod
 {
@@ -24,11 +12,6 @@ namespace XahlicemMod
 				AutoloadGores = true,
 				AutoloadSounds = true
 			};
-		}
-
-		public override void Load() {
-			if (!Main.dedServ)
-				AddEquipTexture(null, EquipType.Legs, "MoonlightLeggings_Legs", "XahlicemMod/Items/MoonlightLeggings_Legs");
 		}
 	}
 }
