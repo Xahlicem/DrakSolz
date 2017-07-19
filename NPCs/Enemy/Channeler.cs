455aa58804cc5a915cab904e1bcb9faf3829c977
Xahlicem <xahlicem@gmail.com>
2017-07-18 23:21:12 -0700

    Moved NPCs around and fixed some code.

--- NPCs/Enemy/Channeler.cs
+++ NPCs/Enemy/Channeler.cs
@@ -4,19 +4,15 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace XahlicemMod.NPCs
-{
+namespace XahlicemMod.NPCs.Enemy {
     // This ModNPC serves as an example of a complete AI example.
-    public class ChannelerDance : ModNPC
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Channeler");
+    public class Channeler : ModNPC {
+        public override void SetStaticDefaults () {
+            DisplayName.SetDefault ("Channeler");
             Main.npcFrameCount[npc.type] = 7; // make sure to set this for your modnpcs.
         }
 
-        public override void SetDefaults()
-        {
+        public override void SetDefaults () {
             npc.width = 40;
             npc.height = 56;
             npc.aiStyle = -1; // This npc has a completely unique AI, so we set this to -1.
@@ -40,8 +36,7 @@ namespace XahlicemMod.NPCs
             npc.ai[3] = -1f;
         }
 
-        public override float SpawnChance(NPCSpawnInfo spawnInfo)
-        {
+        public override float SpawnChance (NPCSpawnInfo spawnInfo) {
             // we would like this npc to spawn in the overworld.
             return SpawnCondition.Overworld.Chance * 5.9f;
         }
@@ -69,20 +64,17 @@ namespace XahlicemMod.NPCs
         // Without it, every instance of "AI_State" in the AI code below would be "npc.ai[AI_State_Slot]". 
         // Also note that without the "AI_State_Slot" defined above, this would be "npc.ai[0]".
         // This is all to just make beautiful, manageable, and clean code.
-        public float AI_State
-        {
+        public float AI_State {
             get { return npc.ai[AI_State_Slot]; }
             set { npc.ai[AI_State_Slot] = value; }
         }
 
-        public float AI_Timer
-        {
+        public float AI_Timer {
             get { return npc.ai[AI_Timer_Slot]; }
             set { npc.ai[AI_Timer_Slot] = value; }
         }
 
-        public float AI_SpellTime
-        {
+        public float AI_SpellTime {
             get { return npc.ai[AI_Dance_Time_Slot]; }
             set { npc.ai[AI_Dance_Time_Slot] = value; }
         }
@@ -90,113 +82,92 @@ namespace XahlicemMod.NPCs
         // AdvancedFlutterSlime will need: float in water, diminishing aggo, spawn projectiles.
 
         // Our AI here makes our NPC sit waiting for a player to enter range, jumps to attack, flutter mid-fall to stay afloat a little longer, then falls to the ground. Note that animation should happen in FindFrame
-        public override void AI()
-        {
+        public override void AI () {
             // The npc starts in the asleep state, waiting for a player to enter range
-            if (AI_State == State_Dance)
-            {
+            if (AI_State == State_Dance) {
                 float distance = 800f;
                 float closest = 1000f, last = 800f;
                 Player p;
 
-                for (int k = 0; k < 200; k++)
-                {
+                for (int k = 0; k < 200; k++) {
                     p = Main.player[k];
-                    if (p.active)
-                    {
-                        if (npc.WithinRange(p.Center, distance))
-                        {
-                            if ((last = npc.Distance(p.Center)) < closest && last >= 150)
-                            {
+                    if (p.active) {
+                        if (npc.WithinRange (p.Center, distance)) {
+                            if ((last = npc.Distance (p.Center)) < closest && last >= 150) {
                                 closest = last;
                                 npc.localAI[0] = p.position.X;
                                 npc.localAI[1] = p.position.Y - 24;
                                 npc.ai[3] = 1f;
                             }
-                            p.AddBuff(BuffID.WitheredArmor, 300);
-                            p.AddBuff(BuffID.WitheredWeapon, 300);
-                            p.AddBuff(BuffID.WaterCandle, 300);
+                            p.AddBuff (BuffID.WitheredArmor, 300);
+                            p.AddBuff (BuffID.WitheredWeapon, 300);
+                            p.AddBuff (BuffID.WaterCandle, 300);
                         }
                     }
                 }
 
                 // TargetClosest sets npc.target to the player.whoAmI of the closest player. the faceTarget parameter means that npc.direction will automatically be 1 or -1 if the targetted player is to the right or left. This is also automatically flipped if npc.confused
-                npc.TargetClosest(true);
+                npc.TargetClosest (true);
                 // Now we check the make sure the target is still valid and within our specified notice range (500)
-                if (npc.HasValidTarget && Main.player[npc.target].Distance(npc.Center) < 350f)
-                {
+                if (npc.HasValidTarget && Main.player[npc.target].Distance (npc.Center) < 350f) {
                     // Since we have a target in range, we change to the Notice state. (and zero out the Timer for good measure)
                     AI_State = State_Spell;
                     AI_Timer = 0;
                 }
             }
             // In this state, a player has been targeted
-            else if (AI_State == State_Spell)
-            {
+            else if (AI_State == State_Spell) {
                 /// If the targeted player is in attack range (250).
                 AI_Timer++;
-                if (Main.player[npc.target].Distance(npc.Center) < 100f)
-                {
-                    if (AI_Timer >= 60)
-                    {
+                if (Main.player[npc.target].Distance (npc.Center) < 100f) {
+                    if (AI_Timer >= 60) {
                         AI_State = State_Teleport;
                     }
-                }
-                else
-                {
-                    npc.TargetClosest(true);
-                    if (!npc.HasValidTarget || Main.player[npc.target].Distance(npc.Center) > 350f)
-                    {
+                } else {
+                    npc.TargetClosest (true);
+                    if (!npc.HasValidTarget || Main.player[npc.target].Distance (npc.Center) > 350f) {
                         // Out targetted player seems to have left our range, so we'll go back to sleep.
                         AI_State = State_Dance;
                         AI_Timer = 0;
                     }
 
-
-                    if (AI_Timer >= 60)
-                    {
+                    if (AI_Timer >= 60) {
                         Vector2 speed = Main.player[npc.target].Center - npc.Center;
-                        AdjustMagnitude(ref speed);
+                        AdjustMagnitude (ref speed);
                         AI_Timer = 0;
-                        if (Main.netMode != 1)
-                        {
-                            Projectile.NewProjectile(npc.Center.X + 6, npc.Center.Y - 16, speed.X, speed.Y, mod.ProjectileType("SoulSpearProjHostile"), npc.damage, 0f);
+                        if (Main.netMode != 1) {
+                            Projectile.NewProjectile (npc.Center.X + 6, npc.Center.Y - 16, speed.X, speed.Y, mod.ProjectileType ("SoulSpearProj"), npc.damage, 0f);
                         }
                     }
                 }
             }
             // In this state, we are in the jump. 
-            else if (AI_State == State_Teleport)
-            {
+            else if (AI_State == State_Teleport) {
                 Vector2 pos;
-                if (npc.ai[3] == 1f) pos = new Vector2(npc.localAI[0], npc.localAI[1]);
-                else if (npc.ai[3] == 0f && npc.Distance(new Vector2(npc.localAI[2], npc.localAI[3])) >= 150) pos = new Vector2(npc.localAI[2], npc.localAI[3]);
-                else pos = new Vector2(npc.position.X + ((Main.rand.NextBool()) ? +200 : -200), npc.position.Y);
+                if (npc.ai[3] == 1f) pos = new Vector2 (npc.localAI[0], npc.localAI[1]);
+                else if (npc.ai[3] == 0f && npc.Distance (new Vector2 (npc.localAI[2], npc.localAI[3])) >= 150) pos = new Vector2 (npc.localAI[2], npc.localAI[3]);
+                else pos = new Vector2 (npc.position.X + ((Main.rand.NextBool ()) ? +200 : -200), npc.position.Y);
                 npc.localAI[2] = npc.position.X;
                 npc.localAI[3] = npc.position.Y;
-                npc.Teleport(pos, 0, 0);
+                npc.Teleport (pos, 0, 0);
                 npc.ai[3] = 0f;
                 AI_State = State_Spell;
             }
             // In this state, our npc starts to flutter/fly a little to make it's movement a little bit interesting.
-            else if (AI_State == State_Still)
-            {
+            else if (AI_State == State_Still) {
                 AI_Timer += 1;
                 // Here we make a decision on how long this flutter will last. We check netmode != 1 to prevent Multiplayer Clients from running this code. (similiarly, spawning projectiles should also be wrapped like this)
                 // netmode == 0 is SP, netmode == 1 is MP Client, netmode == 2 is MP Server. 
                 // Typically in MP, Client and Server maintain the same state by running deterministic code individually. When we want to do something random, we must do that on the server and then inform MP Clients.
                 // Informing MP Clients is done automatically by syncing the npc.ai array over the network whenever npc.netUpdate is set. Don't set netUpdate unless you do something non-deterministic ("random")
-                if (AI_Timer == 1)
-                {
+                if (AI_Timer == 1) {
                     // For reference: without proper syncing: https://gfycat.com/BackAnxiousFerret and with proper syncing: https://gfycat.com/TatteredKindlyDalmatian
                     AI_SpellTime = 50;
                     npc.netUpdate = true;
                 }
                 // Here we add a tiny bit of upward velocity to our npc.
 
-
-                if (AI_Timer > AI_SpellTime)
-                {
+                if (AI_Timer > AI_SpellTime) {
                     // after fluttering for 100 ticks (1.66 seconds), our Flutter Slime is tired, so he decides to go into the Fall state.
                     AI_State = State_Dance;
                     AI_Timer = 0;
@@ -216,31 +187,23 @@ namespace XahlicemMod.NPCs
 
         // Here in FindFrame, we want to set the animation frame our npc will use depending on what it is doing.
         // We set npc.frame.Y to x * frameHeight where x is the xth frame in our spritesheet, counting from 0. For convinience, I have defined some consts above.
-        public override void FindFrame(int frameHeight)
-        {
+        public override void FindFrame (int frameHeight) {
             // This makes the sprite flip horizontally in conjunction with the npc.direction.
             npc.spriteDirection = npc.direction;
 
             // For the most part, our animation matches up with our states.
-            if (AI_State == State_Teleport)
-            {
+            if (AI_State == State_Teleport) {
                 // npc.frame.Y is the goto way of changing animation frames. npc.frame starts from the top left corner in pixel coordinates, so keep that in mind.
                 npc.frame.Y = Frame_Teleport * frameHeight;
-            }
-            else if (AI_State == State_Spell)
-            {
+            } else if (AI_State == State_Spell) {
                 AI_SpellTime++;
                 // Going from Notice to Asleep makes our npc look like it's crouching to jump.
-                if (AI_SpellTime < 10)
-                {
+                if (AI_SpellTime < 10) {
                     npc.frame.Y = Frame_Teleport * frameHeight;
-                }
-                else
-                {
+                } else {
                     npc.frame.Y = Frame_Spell * frameHeight;
                 }
-                if (AI_SpellTime >= 60)
-                {
+                if (AI_SpellTime >= 60) {
                     //Vector2 speed = Main.player[npc.target].Center - npc.Center;
                     //AdjustMagnitude(ref speed);
                     AI_SpellTime = 0;
@@ -249,72 +212,42 @@ namespace XahlicemMod.NPCs
                     //    Projectile.NewProjectile(npc.Center.X + 6, npc.Center.Y - 16, speed.X, speed.Y, mod.ProjectileType("SoulSpearProjHostile"), 15, 0f);
                     //}
                 }
-            }
-            else if (AI_State == State_Dance)
-            {
+            } else if (AI_State == State_Dance) {
                 // Here we have 3 frames that we want to cycle through.
 
                 npc.frameCounter++;
-                if (npc.frameCounter < 8)
-                {
-                    npc.velocity = new Vector2(0, -1);
+                if (npc.frameCounter < 8) {
+                    npc.velocity = new Vector2 (0, -1);
                     npc.frame.Y = Frame_Dance_1 * frameHeight;
-                }
-                else if (npc.frameCounter < 16)
-                {
+                } else if (npc.frameCounter < 16) {
                     npc.frame.Y = Frame_Dance_2 * frameHeight;
-                }
-                else if (npc.frameCounter < 24)
-                {
-                    npc.velocity = new Vector2(0, -1);
+                } else if (npc.frameCounter < 24) {
+                    npc.velocity = new Vector2 (0, -1);
                     npc.frame.Y = Frame_Dance_1 * frameHeight;
-                }
-                else if (npc.frameCounter < 32)
-                {
+                } else if (npc.frameCounter < 32) {
                     npc.frame.Y = Frame_Dance_2 * frameHeight;
-                }
-                else if (npc.frameCounter < 40)
-                {
+                } else if (npc.frameCounter < 40) {
                     npc.frame.Y = Frame_Dance_1 * frameHeight;
-                }
-                else if (npc.frameCounter < 48)
-                {
+                } else if (npc.frameCounter < 48) {
                     npc.frame.Y = Frame_Dance_3 * frameHeight;
-                }
-                else if (npc.frameCounter < 56)
-                {
-                    npc.velocity = new Vector2(0, -1);
+                } else if (npc.frameCounter < 56) {
+                    npc.velocity = new Vector2 (0, -1);
                     npc.frame.Y = Frame_Dance_4 * frameHeight;
-                }
-                else if (npc.frameCounter < 64)
-                {
+                } else if (npc.frameCounter < 64) {
                     npc.frame.Y = Frame_Dance_5 * frameHeight;
-                }
-                else if (npc.frameCounter < 72)
-                {
-                    npc.velocity = new Vector2(0, -1);
+                } else if (npc.frameCounter < 72) {
+                    npc.velocity = new Vector2 (0, -1);
                     npc.frame.Y = Frame_Dance_4 * frameHeight;
-                }
-                else if (npc.frameCounter < 80)
-                {
+                } else if (npc.frameCounter < 80) {
                     npc.frame.Y = Frame_Dance_5 * frameHeight;
-                }
-                else if (npc.frameCounter < 88)
-                {
+                } else if (npc.frameCounter < 88) {
                     npc.frame.Y = Frame_Dance_4 * frameHeight;
-                }
-                else if (npc.frameCounter < 96)
-                {
+                } else if (npc.frameCounter < 96) {
                     npc.frame.Y = Frame_Dance_6 * frameHeight;
-                }
-                else
-                {
+                } else {
                     npc.frameCounter = 0;
                 }
-            }
-
-            else if (AI_State == State_Still)
-            {
+            } else if (AI_State == State_Still) {
                 // Going from Notice to Asleep makes our npc look like it's crouching to jump.
                 npc.frame.Y = Frame_Teleport * frameHeight;
 
@@ -322,12 +255,9 @@ namespace XahlicemMod.NPCs
 
         }
 
-
-        private void AdjustMagnitude(ref Vector2 vector)
-        {
-            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
-            if (magnitude > 7.5f)
-            {
+        private void AdjustMagnitude (ref Vector2 vector) {
+            float magnitude = (float) Math.Sqrt (vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude > 7.5f) {
                 vector *= 7.5f / magnitude;
             }
         }
---
+++ NPCs/Town/Pilgrim.cs
@@ -0,0 +1,182 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace XahlicemMod.NPCs.Town {
+    [AutoloadHead]
+    public class Pilgrim : ModNPC {
+        /*public override string Texture
+        {
+            get
+            {
+                return "XahlicemMod/NPCs/Pilgrim";
+            }
+        }
+
+        public override string[] AltTextures
+        {
+            get
+            {
+                return new string[] { "XahlicemMod/NPCs/Pilgrim_Alt_1" };
+            }
+        }*/
+
+        public override bool Autoload (ref string name) {
+            name = "Pilgrim";
+            return mod.Properties.Autoload;
+        }
+
+        public override void SetStaticDefaults () {
+            DisplayName.SetDefault ("Pilgrim");
+            Main.npcFrameCount[npc.type] = 25;
+            NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+            NPCID.Sets.AttackFrameCount[npc.type] = 4;
+            NPCID.Sets.DangerDetectRange[npc.type] = 700;
+            NPCID.Sets.AttackType[npc.type] = 0;
+            NPCID.Sets.AttackTime[npc.type] = 90;
+            NPCID.Sets.AttackAverageChance[npc.type] = 30;
+            NPCID.Sets.HatOffsetY[npc.type] = 4;
+        }
+
+        public override void SetDefaults () {
+            npc.townNPC = true;
+            npc.friendly = true;
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 7;
+            npc.damage = 10;
+            npc.defense = 15;
+            npc.lifeMax = 250;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.knockBackResist = 0.5f;
+            animationType = NPCID.Guide;
+        }
+
+        public override void HitEffect (int hitDirection, double damage) {
+            int num = npc.life > 0 ? 1 : 5;
+            for (int k = 0; k < num; k++) {
+                Dust.NewDust (npc.position, npc.width, npc.height, DustID.Smoke);
+            }
+        }
+
+        public override bool CanTownNPCSpawn (int numTownNPCs, int money) {
+            bool soul = false, lantern = false;
+            for (int k = 0; k < 255; k++) {
+                Player player = Main.player[k];
+                if (player.active) {
+                    for (int j = 0; j < player.inventory.Length; j++) {
+                        if (player.inventory[j].type == mod.ItemType ("Soul")) soul = true;
+                        if (player.inventory[j].type == mod.ItemType ("SkullLantern")) lantern = true;
+                    }
+                }
+            }
+            return soul && lantern;
+        }
+
+        public override bool CheckConditions (int left, int right, int top, int bottom) {
+            return true;
+        }
+
+        public override string TownNPCName () {
+            switch (WorldGen.genRand.Next (4)) {
+                case 0:
+                    return "Yoel";
+                case 1:
+                    return "Anri";
+                case 2:
+                    return "Artorias";
+                default:
+                    return "Gael";
+            }
+        }
+
+        public override string GetChat () {
+            int partyGirl = NPC.FindFirstNPC (NPCID.PartyGirl);
+            if (partyGirl >= 0 && Main.rand.Next (4) == 0) {
+                return "Can you please tell " + Main.npc[partyGirl].GivenName + " to stop decorating my house with colors?";
+            }
+            switch (Main.rand.Next (3)) {
+                case 0:
+                    return "Oh, sweet champion, you've returned.";
+                case 1:
+                    return "May the dark sigil guide your way.";
+                default:
+                    return "Stay safe champion.";
+            }
+        }
+
+        /* 
+		// Consider using this alternate approach to choosing a random thing. Very useful for a variety of use cases.
+		// The WeightedRandom class needs "using Terraria.Utilities;" to use
+		public override string GetChat ()
+		{
+			WeightedRandom<string> chat = new WeightedRandom<string> ();
+			int partyGirl = NPC.FindFirstNPC (NPCID.PartyGirl);
+			if (partyGirl >= 0 && Main.rand.Next (4) == 0)
+			{
+				chat.Add ("Can you please tell " + Main.npc[partyGirl].GivenName + " to stop decorating my house with colors?");
+			}
+			chat.Add ("Sometimes I feel like I'm different from everyone else here.");
+			chat.Add ("What's your favorite color? My favorite colors are white and black.");
+			chat.Add ("What? I don't have any arms or legs? Oh, don't be ridiculous!");
+			chat.Add ("This message has a weight of 5, meaning it appears 5 times more often.", 5.0);
+			chat.Add ("This message has a weight of 0.1, meaning it appears 10 times as rare.", 0.1);
+			return chat; // chat is implicitly cast to a string. You can also do "return chat.Get();" if that makes you feel better
+		}
+		*/
+
+        public override void SetChatButtons (ref string button, ref string button2) {
+            button = Lang.inter[28].Value;
+        }
+
+        public override void OnChatButtonClicked (bool firstButton, ref bool shop) {
+            if (firstButton) {
+                shop = true;
+            }
+        }
+
+        public override void SetupShop (Chest shop, ref int nextSlot) {
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("Eater1"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("Leecher1"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingFavor"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingCalamity"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingRedTear"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingBlueTear"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingCloranthy"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingCat"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("RingSteelProt"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults (mod.ItemType ("MorianBlade"));
+            nextSlot++;
+        }
+
+        public override void TownNPCAttackStrength (ref int damage, ref float knockback) {
+            damage = 20;
+            knockback = 4f;
+        }
+
+        public override void TownNPCAttackCooldown (ref int cooldown, ref int randExtraCooldown) {
+            cooldown = 30;
+            randExtraCooldown = 30;
+        }
+
+        public override void TownNPCAttackProj (ref int projType, ref int attackDelay) {
+            projType = ProjectileID.BoneDagger;
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed (ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
+            multiplier = 12f;
+            randomOffset = 2f;
+        }
+    }
+}
