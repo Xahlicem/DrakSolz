7ca051fa80cb02416a35aab95b630e404900c4a2
Xahlicem <xahlicem@gmail.com>
2017-08-17 15:20:19 -0700

    Rnamed. Made fishlike.

--- Buffs/Race/LizardMan.cs
+++ Buffs/Race/LizardMan.cs
@@ -7,9 +7,9 @@ namespace XahlicemMod.Buffs.Race
 
 {
 
-    public class LizardMan : ModBuff {
+    public class Lizardman : ModBuff {
         public override void SetDefaults() {
-            DisplayName.SetDefault("LizardMan");
+            DisplayName.SetDefault("Lizardman");
             Description.SetDefault("Power of cold blooded!");
             Main.debuff[Type] = false;
             Main.buffNoSave[Type] = false;
@@ -24,18 +24,24 @@ namespace XahlicemMod.Buffs.Race
             player.statLifeMax2 = (int)(player.statLifeMax2 * 1.2);
             player.lifeRegenTime = 2;
             player.lifeRegen += player.statLifeMax2 / 50;
-            if (Main.time % 12 == 0) player.breath += 1;
+            player.breath -= 3;
 
-            if (Main.dayTime) {
+            if (Main.dayTime || modPlayer.wet) {
                 player.moveSpeed *= 1.25f;
                 player.maxRunSpeed *= 1.25f;
                 player.accRunSpeed *= 1.25f;
             } else {
-                    player.moveSpeed *= 0.75f;
-                    player.maxRunSpeed *= 0.75f;
-                    player.accRunSpeed = 0;
+                player.moveSpeed *= 0.75f;
+                player.maxRunSpeed *= 0.75f;
+                player.accRunSpeed = 0;
             }
-            
+
+            player.detectCreature = true;
+            if (!modPlayer.wet && player.FindBuffIndex(BuffID.Gills) == -1 && player.armor[0].type != ItemID.FishBowl) {
+                player.breathCD = 0;
+                if (Main.time % 5 == 0) player.breath -= 1;
+            } else player.breath += 4;
+            if (player.breath <= 0) player.AddBuff(BuffID.Suffocation, 10);
         }
     }
 }
