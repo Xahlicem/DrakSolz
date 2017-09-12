5b9153c3628152eb91e59f1e2910129836fed55c
Xahlicem <xahlicem@gmail.com>
2017-09-12 14:16:30 -0700

    Life and Mana Crystals usable!

--- Buffs/Firelink.cs
+++ Buffs/Firelink.cs
@@ -18,11 +18,13 @@ namespace XahlicemMod.Buffs
         }
 
         public override void Update(Player player, ref int buffIndex) {
-            player.headcovered = true;
             if (Main.time % 30 == 0 && player.statLife < player.statLifeMax2) {
-                player.statLife += (int)(player.statLifeMax2 * 0.025);
-                player.HealEffect((int)(player.statLifeMax2 * 0.025));
+                player.statLife += (int)(player.statLifeMax2 * 0.01);
+                player.HealEffect((int)(player.statLifeMax2 * 0.01));
             }
+
+            int index = player.FindBuffIndex(mod.BuffType<Buffs.Hollow>());
+            if (index != -1) player.buffTime[index]--;
         }
     }
 }
--- Items/XItem.cs
+++ Items/XItem.cs
@@ -90,8 +90,50 @@ namespace XahlicemMod.Items {
 
     public class ManaHealth : GlobalItem {
         public override bool CanUseItem(Item item, Player player) {
-            if (item.type == ItemID.LifeCrystal || item.type == ItemID.LifeFruit || item.type == ItemID.ManaCrystal) return false;
+            if (item.type == ItemID.LifeCrystal || item.type == ItemID.LifeFruit || item.type == ItemID.ManaCrystal) return true;
             else return base.ConsumeItem(item, player);
+        }
+
+        public override void ModifyTooltips(Item item, System.Collections.Generic.List<TooltipLine> tooltips) {
+            if (item.type == ItemID.LifeCrystal) {
+                tooltips[tooltips.Capacity - 1].text = "Makes you whole and increases life regeneration for 5 minutes";
+            }
+            if (item.type == ItemID.LifeFruit) {
+                tooltips[tooltips.Capacity - 2].text = "Makes you whole and increases life regeneration for 1 minute";
+            }
+            if (item.type == ItemID.ManaCrystal) {
+                tooltips[tooltips.Capacity - 2].text = "Increases mana regeneration and magic damage for 1 minute";
+            }
+        }
+
+        public override bool ConsumeItem(Item item, Player player) {
+            if (item.type == ItemID.LifeCrystal || item.type == ItemID.LifeFruit) {
+                player.AddBuff(BuffID.Regeneration, 60 * 60 * ((item.type == ItemID.LifeCrystal)?5:1));
+
+                int index = player.FindBuffIndex(mod.BuffType<Buffs.Hollow>());
+                if (index != -1) player.buffTime[index] = 0;
+            }
+            if (item.type == ItemID.ManaCrystal) {
+                player.AddBuff(BuffID.ManaRegeneration, 60 * 60 * 1);
+                player.AddBuff(BuffID.MagicPower, 60 * 60 * 1);
+            }
+            return base.ConsumeItem(item, player);
+        }
+
+        public override bool UseItem(Item item, Player player) {
+            if (item.type == ItemID.LifeCrystal || item.type == ItemID.LifeFruit) {
+                player.AddBuff(BuffID.Regeneration, 60 * 60 * ((item.type == ItemID.LifeCrystal)?5:1));
+
+                int index = player.FindBuffIndex(mod.BuffType<Buffs.Hollow>());
+                if (index != -1) player.buffTime[index] = 0;
+                return true;
+            }
+            if (item.type == ItemID.ManaCrystal) {
+                player.AddBuff(BuffID.ManaRegeneration, 60 * 60 * 1);
+                player.AddBuff(BuffID.MagicPower, 60 * 60 * 1);
+                return true;
+            }
+            return base.ConsumeItem(item, player);
         }
     }
 }
