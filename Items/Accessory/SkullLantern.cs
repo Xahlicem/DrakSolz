e948087958528467af71dd72aaa93f5aeace3e96
Xahlicem <xahlicem@gmail.com>
2017-09-14 17:09:29 -0700

    Fixed position.

--- Items/Accessory/SkullLantern.cs
+++ Items/Accessory/SkullLantern.cs
@@ -21,8 +21,9 @@ namespace XahlicemMod.Items.Accessory {
             item.rare = 2;
             item.accessory = true;
             item.autoReuse = true;
+            item.useTurn = true;
         }
-        //these wings use the same values as the solar wings
+
         public override void UpdateAccessory(Player player, bool hideVisual) {
             player.AddBuff(BuffID.Warmth, 2);
             player.buffImmune[BuffID.Chilled] = true;
@@ -37,24 +38,21 @@ namespace XahlicemMod.Items.Accessory {
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
+
+        public override void UseStyle(Player player) {
+            player.itemLocation.Y += 20;
+            player.itemLocation.X += player.direction == 1 ? -10 : 10;
+        }
+
         public override bool UseItem(Player player) {
-            float offset = player.direction == 1 ? 21 : -21;
-            for (int i = 0; i < 10; i++)
-
-            {
-
-                int dustIndex = Dust.NewDust(new Vector2(player.position.X + offset, player.position.Y), item.width, item.height, 6, 0f, 0f, 100, default(Color), 2f);
-
+            float offset = player.direction == 1 ? 11 : -11;
+            for (int i = 0; i < 10; i++) {
+                int dustIndex = Dust.NewDust(new Vector2(player.position.X + offset, player.position.Y + 20), item.width, item.height, 6, 0f, 0f, 100, default(Color), 2f);
                 Main.dust[dustIndex].noGravity = true;
-
                 Main.dust[dustIndex].scale *= 0.6f;
-
-                dustIndex = Dust.NewDust(new Vector2(player.position.X + offset, player.position.Y), item.width, item.height, 6, 0f, 0f, 100, default(Color), 1f);
-
+                dustIndex = Dust.NewDust(new Vector2(player.position.X + offset, player.position.Y + 20), item.width, item.height, 6, 0f, 0f, 100, default(Color), 1f);
                 Main.dust[dustIndex].velocity *= 0.5f;
-
                 Main.dust[dustIndex].scale *= 0.6f;
-
             }
             return true;
         }
