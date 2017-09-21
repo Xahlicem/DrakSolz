801ef580682f6f9a33d3a097686d204923dac900
SolarEclipse28 <areon_blade@yahoo.com>
2017-09-21 16:40:45 -0700

    Item complete

--- Items/Magic/IT2.cs
+++ Items/Magic/IT2.cs
@@ -14,19 +14,55 @@ namespace DrakSolz.Items.Magic {
         public override void SetDefaults() {
             //item.CloneDefaults(ItemID.StardustDragonStaff);
             item.useStyle = 1;
-            item.damage = 70;
-            item.useTime = 40;
-            item.useAnimation = 40;
-            item.mana = 15;
-            item.knockBack = 0f;
+            item.magic = true;
+            item.damage = 80;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.rare = 10;
+            item.mana = 20;
+            item.knockBack = 18f;
             item.shootSpeed = 0f;
-            item.autoReuse = true;
+            item.autoReuse = false;
             item.shoot = mod.ProjectileType<Projectiles.Magic.FlameMageProj1>();
+            item.buffType = BuffID.OnFire;
+            item.buffTime = 120;
         }
 
+        public override bool CanUseItem(Player player) {
+            if (item.mana == 0) item.mana = item.alpha;
+            else item.alpha = item.mana;
+            item.buffTime = item.mana;
+            item.mana = 0;
+            return base.CanUseItem(player);
+        }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
-            int pro = Projectile.NewProjectile(Main.mouseX + Main.screenPosition.X, Main.mouseY + Main.screenPosition.Y, 0, 40, type, damage, knockBack, player.whoAmI);
+            if (player.statMana >= item.buffTime * player.manaCost) {
+                damage *= 2;
+                player.statMana -= (int)(item.buffTime * player.manaCost);
+                item.mana = item.buffTime;
+                int pro = Projectile.NewProjectile(Main.mouseX + Main.screenPosition.X, Main.mouseY + Main.screenPosition.Y, 0, 40, type, (int)(damage * 0.5f), 0, player.whoAmI);
+                return false;
+            }
+            item.mana = item.buffTime;
             return false;
         }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.Next(1) == 0)
+			{
+				//Emit dusts when swing the sword
+				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Fire);
+                Main.dust[dust].scale *= 1.5f + Main.rand.NextFloat();
+                Main.dust[dust].noGravity = true;
+			}
+		}
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+		{
+			// Add Onfire buff to the NPC for 1 second
+			// 60 frames = 1 second
+			target.AddBuff(BuffID.OnFire, 120);		
+}
     }
 }
