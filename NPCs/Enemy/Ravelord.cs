8e683894fb02a67e90c543f43b955be67494e6ab
SolarEclipse28 <areon_blade@yahoo.com>
2017-09-26 15:29:18 -0700

    Added Ravelord NPC

---
+++ NPCs/Enemy/Ravelord.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.NPCs.Enemy {
+    // This ModNPC serves as an example of a complete AI example.
+    public class Ravelord : ModNPC {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Ravelord");
+            Main.npcFrameCount[npc.type] = 17; // make sure to set this for your modnpcs.
+        }
+
+        public override void SetDefaults() {
+            npc.width = 55;
+            npc.scale *= 1.5f;
+            npc.height = 65;
+            npc.aiStyle = 3;
+            aiType = NPCID.PossessedArmor;
+            animationType = NPCID.PossessedArmor;
+            npc.damage = 115;
+            npc.defense = 50;
+            npc.lifeMax = 2500;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            //npc.alpha = 175;
+            //npc.color = new Color(0, 80, 255, 100);
+            npc.value = 50000f;
+            npc.knockBackResist = 0.1f;
+            banner = npc.type;
+            bannerItem = mod.ItemType<Items.Banners.RavelordBanner>();
+            npc.buffImmune[BuffID.Confused] = false; // npc default to being immune to the Confused debuff. Allowing confused could be a little more work depending on the AI. npc.confused is true while the npc is confused.
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+            if (NPC.downedGolemBoss) {
+                return SpawnCondition.Cavern.Chance * 0.2f;
+            } else return 0f;
+        }
+        public override void FindFrame(int frameHeight) {
+            // This makes the sprite flip horizontally in conjunction with the npc.direction.
+            npc.spriteDirection = npc.direction;
+        }
+        public override void NPCLoot() {
+            if (Main.rand.Next(15) == 0) Item.NewItem(npc.position, npc.width, npc.height, mod.ItemType<Items.Melee.GravelordSword>());
+        }
+
+    }
+}
