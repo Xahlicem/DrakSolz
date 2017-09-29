85404cc9ac429e83d38f62f47ce9038ff0a1c776
Xahlicem <xahlicem@gmail.com>
2017-09-29 10:28:44 -0700

    Drops from WoF.

--- Items/Accessory/ThiefEmblem.cs
+++ Items/Accessory/ThiefEmblem.cs
@@ -1,5 +1,5 @@
-﻿using Terraria;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -23,7 +23,7 @@ namespace DrakSolz.Items.Accessory {
         }
         public override void AddRecipes() {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(mod.ItemType<Items.Accessory.ThiefEmblem>());        
+            recipe.AddIngredient(mod.ItemType<Items.Accessory.ThiefEmblem>());
             recipe.AddIngredient(ItemID.SoulofSight, 5);
             recipe.AddIngredient(ItemID.SoulofMight, 5);
             recipe.AddIngredient(ItemID.SoulofFright, 5);
@@ -31,20 +31,53 @@ namespace DrakSolz.Items.Accessory {
             recipe.AddRecipe();
             recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.WarriorEmblem);
-			recipe.SetResult(this);
+            recipe.SetResult(this);
             recipe.AddRecipe();
             recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.SorcererEmblem);
-			recipe.SetResult(this);
+            recipe.SetResult(this);
             recipe.AddRecipe();
             recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.RangerEmblem);
-			recipe.SetResult(this);
+            recipe.SetResult(this);
             recipe.AddRecipe();
             recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.SummonerEmblem);
-			recipe.SetResult(this);
+            recipe.SetResult(this);
             recipe.AddRecipe();
+        }
+    }
+
+    public class ThiefEmblemDrop : GlobalNPC {
+        public override bool PreNPCLoot(NPC npc) {
+            if (npc.type == NPCID.WallofFlesh) {
+                int[] weapons = { ItemID.BreakerBlade, ItemID.ClockworkAssaultRifle, ItemID.LaserRifle };
+                int[] emblems = { ItemID.WarriorEmblem, ItemID.SorcererEmblem, ItemID.RangerEmblem, ItemID.SummonerEmblem, mod.ItemType<Items.Accessory.ThiefEmblem>() };
+                if (Main.rand.NextBool())
+                    Item.NewItem(npc.position, npc.width, npc.height, Utils.SelectRandom(Main.rand, weapons));
+                else
+                    Item.NewItem(npc.position, npc.width, npc.height, Utils.SelectRandom(Main.rand, emblems));
+                foreach (int w in weapons) NPCLoader.blockLoot.Add(w);
+                foreach (int e in emblems) NPCLoader.blockLoot.Add(e);
+            }
+            return true;
+        }
+    }
+
+    public class ThiefEmblemBag : GlobalItem {
+        public override bool PreOpenVanillaBag(string context, Player player, int arg) {
+            if (context == "bossBag" && arg == ItemID.WallOfFleshBossBag) {
+                int[] weapons = { ItemID.BreakerBlade, ItemID.ClockworkAssaultRifle, ItemID.LaserRifle };
+                int[] emblems = { ItemID.WarriorEmblem, ItemID.SorcererEmblem, ItemID.RangerEmblem, ItemID.SummonerEmblem, mod.ItemType<Items.Accessory.ThiefEmblem>() };
+
+                if (!player.extraAccessory) player.QuickSpawnItem(ItemID.DemonHeart);
+                if (Main.rand.Next(7) == 0) player.QuickSpawnItem(ItemID.FleshMask);
+                player.QuickSpawnItem(ItemID.Pwnhammer);
+                player.QuickSpawnItem(Utils.SelectRandom(Main.rand, weapons));
+                player.QuickSpawnItem(Utils.SelectRandom(Main.rand, emblems));
+                player.QuickSpawnItem(ItemID.GoldCoin, Main.rand.Next(6, 10));
+                return false;
+            } else return true;
         }
     }
 }
