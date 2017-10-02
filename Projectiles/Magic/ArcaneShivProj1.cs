cba49f039c4bb166a3d6250956d52f09acbe6a56
SolarEclipse28 <areon_blade@yahoo.com>
2017-10-02 15:25:54 -0700

    Added Illusory Shiv.

---
+++ Projectiles/Magic/ArcaneShivProj1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.Projectiles.Magic {
+    public class ArcaneShivProj1 : ModProjectile {
+
+        public override void SetStaticDefaults() {
+            //ProjectileID.Sets.Homing[projectile.type] = false;
+            Main.projFrames[projectile.type] = 1;
+        }
+
+        public override void SetDefaults() {
+            projectile.CloneDefaults(ProjectileID.MagicDagger);
+        }
+        public override void AI() {
+            /*int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 15, 0, 0, 0, Color.DodgerBlue, 0.5f);
+            Main.dust[dust].velocity *= 1.5f + Main.rand.NextFloat();
+            Main.dust[dust].scale *= 0.5f + Main.rand.NextFloat();
+            Main.dust[dust].noGravity = true;*/
+            if (projectile.timeLeft < 20) {
+                projectile.alpha += 12;
+                projectile.scale -= 0.03f;
+
+            }
+        }
+    }
+}
