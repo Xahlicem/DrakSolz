17517046012ad156321e6f2dd1b769fdbbc4f3ab
SolarEclipse28 <areon_blade@yahoo.com>
2017-10-03 13:52:33 -0700

    Added Giant's Armor. Not yet obtainable.

---
+++ Items/Armor/GiantArmor.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.Items.Armor {
+    [AutoloadEquip(EquipType.Body)]
+    public class GiantArmor : ModItem {
+        public override void SetStaticDefaults() {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Giant's Armor");
+            Tooltip.SetDefault("Become Unstoppable!");
+        }
+
+        public override void SetDefaults() {
+            item.width = 18;
+            item.height = 18;
+            item.value = Item.buyPrice(0, 50, 0, 0);
+            item.rare = 9;
+            item.defense = 35;
+        }
+
+        public override void UpdateEquip(Player player) {
+            player.statManaMax2 += 0;
+            player.statLifeMax2 += 0;
+        }
+    }
+}
