e21ca48632de0a002b871cac30839deb64b7ec14
SolarEclipse28 <areon_blade@yahoo.com>
2017-10-24 16:47:22 -0700

    Updated set bonus.

--- Items/Armor/Void/VoidMask.cs
+++ Items/Armor/Void/VoidMask.cs
@@ -33,54 +33,26 @@ namespace DrakSolz.Items.Armor.Void {
 
         public override void UpdateArmorSet(Player player) {
             player.setBonus = ("Absence" +
-                "\n+Immunity to all debuffs" +
-                "\n+83% chance to not consume thrown weapons" +
-                "\n+");
+                "\nGravity Control" +
+                "\n+83% chance to not consume thrown weapons");
+            player.AddBuff(BuffID.Featherfall, 2);
+            player.gravControl = true;
+            if (player.gravDir == -1) {
+                player.thrownDamage *= 1.30f;
+                player.thrownCrit += 15;
+            }
             player.thrownCost50 = true;
             player.thrownCost33 = true;
-            player.buffImmune[BuffID.Blackout] = true;
-            player.buffImmune[BuffID.Bleeding] = true;
-            player.buffImmune[BuffID.BrokenArmor] = true;
-            player.buffImmune[BuffID.Burning] = true;
-            player.buffImmune[BuffID.ChaosState] = true;
             player.buffImmune[BuffID.Chilled] = true;
-            player.buffImmune[BuffID.Confused] = true;
-            player.buffImmune[BuffID.Cursed] = true;
-            player.buffImmune[BuffID.CursedInferno] = true;
-            player.buffImmune[BuffID.Darkness] = true;
             player.buffImmune[BuffID.Dazed] = true;
-            player.buffImmune[BuffID.Electrified] = true;
-            player.buffImmune[BuffID.Frostburn] = true;
             player.buffImmune[BuffID.Frozen] = true;
-            player.buffImmune[BuffID.Horrified] = true;
-            player.buffImmune[BuffID.Ichor] = true;
-            //player.buffImmune[BuffID.ManaSickness] = true;
-            player.buffImmune[BuffID.MoonLeech] = true;
-            player.buffImmune[BuffID.NoBuilding] = true;
             player.buffImmune[BuffID.OgreSpit] = true;
-            player.buffImmune[BuffID.Oiled] = true;
             player.buffImmune[BuffID.Obstructed] = true;
-            player.buffImmune[BuffID.OnFire] = true;
-            player.buffImmune[BuffID.Poisoned] = true;
-            //player.buffImmune[BuffID.PotionSickness] = true;
-            player.buffImmune[BuffID.Rabies] = true;
-            player.buffImmune[BuffID.ShadowFlame] = true;
-            player.buffImmune[BuffID.Silenced] = true;
             player.buffImmune[BuffID.Slow] = true;
-            player.buffImmune[BuffID.Slimed] = true;
-            player.buffImmune[BuffID.Stinky] = true;
             player.buffImmune[BuffID.Stoned] = true;
-            player.buffImmune[BuffID.Suffocation] = true;
-            player.buffImmune[BuffID.TheTongue] = true;
-            player.buffImmune[BuffID.Venom] = true;
-            player.buffImmune[BuffID.Weak] = true;
             player.buffImmune[BuffID.Webbed] = true;
-            player.buffImmune[BuffID.Wet] = true;
             player.buffImmune[BuffID.WindPushed] = true;
-            player.buffImmune[BuffID.WitheredArmor] = true;
-            player.buffImmune[BuffID.WitheredWeapon] = true;
             player.buffImmune[BuffID.VortexDebuff] = true;
-            player.buffImmune[BuffID.SoulDrain] = true;
         }
         public override void ArmorSetShadows(Player player) {
             player.armorEffectDrawOutlines = true;
