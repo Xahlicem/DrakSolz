dcb713d6824d13fdea62bdc4cde34bea8e962511
SolarEclipse28 <areon_blade@yahoo.com>
2018-07-09 16:51:13 -0700

    Added Images for Holy Spells(Miracles).

---
+++ Items/Magic/Holy/ScrollHolyForce.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace DrakSolz.Items.Magic.Holy {
+    public class ScrollHolyForce : SoulItem {
+        public ScrollHolyForce() : base(100) { }
+
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Force");
+            Tooltip.SetDefault("Pyromancy that projects a Fire Ball toward your target.");
+        }
+
+        public override void SetDefaults() {
+            item.CloneDefaults(ItemID.Flamelash);
+            item.useStyle = 5;
+            item.noUseGraphic = true;
+            item.damage = 6;
+            item.useTime = 40;
+            item.useAnimation = 40;
+            item.mana = 5;
+            item.knockBack = 12f;
+            item.shootSpeed = 2.5f;
+            item.shoot = mod.ProjectileType<Projectiles.ForceProj>();
+        }
+
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new SoulRecipe(mod, this);
+            recipe.AddIngredient(mod.ItemType<Items.Misc.ScrollHoly>());
+            recipe.AddTile(mod.TileType<Tiles.FirelinkShrineTile>());
+            recipe.AddRecipe();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+            int pro = Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+            Main.projectile[pro].frame = 1;
+            return false;
+        }
+    }
+}
---
+++ Projectiles/ForceProj.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.Projectiles {
+    public class ForceProj : ModProjectile {
+
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Force"); //The English name of the projectile
+            Main.projFrames[projectile.type] = 1;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5; //The length of old position to be recorded
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0; //The recording mode
+        }
+
+        public override void SetDefaults() {
+            projectile.width = 8; //The width of projectile hitbox
+            projectile.height = 16; //The height of projectile hitbox
+            projectile.aiStyle = 29; //The ai style of the projectile, please reference the source code of Terraria
+            projectile.friendly = true; //Can the projectile deal damage to enemies?
+            projectile.hostile = false; //Can the projectile deal damage to the player?
+            projectile.magic = true; //Is the projectile shoot by a magic weapon?
+            projectile.penetrate = 1; //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+            projectile.timeLeft = 120; //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+            projectile.alpha = 0; //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
+            //projectile.light = 1.0f;            //How much light emit around the projectile
+            projectile.ignoreWater = true; //Does the projectile's speed be influenced by water?
+            projectile.tileCollide = true; //Can the projectile collide with tiles?
+            projectile.extraUpdates = 1; //Set to above 0 if you want the projectile to update multiple time in a frame
+            projectile.scale *= 1.0f;
+            aiType = ProjectileID.DiamondBolt; //Act exactly like default Bullet
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough) {
+            width = 2;
+            height = 8;
+            return true;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			//If collide with tile, reduce the penetrate.
+			//So the projectile can reflect at most 5 times
+            projectile.penetrate--;
+			if (projectile.penetrate <= 0)
+			{
+				projectile.Kill();
+			}
+			else
+			{
+				if (projectile.velocity.X != oldVelocity.X)
+				{
+					projectile.velocity.X = -oldVelocity.X;
+				}
+				if (projectile.velocity.Y != oldVelocity.Y)
+				{
+					projectile.velocity.Y = -oldVelocity.Y;
+				}
+				Main.PlaySound(SoundID.Item10, projectile.position);
+			}
+			return false;
+}
+        public override void AI() {
+            projectile.rotation = (float) Math.Atan2((double) projectile.velocity.Y, (double) projectile.velocity.X) + MathHelper.ToRadians(90f);
+            if (projectile.spriteDirection == -1) {
+                projectile.rotation -= MathHelper.ToRadians(270f);
+            }
+
+            projectile.ai[0]++;
+        }
+    }
+}
