462d4c8762d4170ef6fb7c69263b69ba9acb79a8
SolarEclipse28 <areon_blade@yahoo.com>
2018-07-17 16:34:36 -0700

    Made spawn ShadeStalker. Altered Stats.

---
+++ Projectiles/StalkerProj.cs
@@ -0,0 +1,32 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.Projectiles {
+    public class StalkerProj : ModProjectile {
+        public override string Texture { get { return "Terraria/Projectile_497"; } }
+
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Stalker");
+        }
+
+        public override void SetDefaults() {
+            projectile.CloneDefaults(ProjectileID.Bone);
+            projectile.aiStyle = 2;
+            projectile.friendly = false;
+            projectile.hostile = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 600;
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough) {
+            width = 5;
+            height = 5;
+            return true;
+        }
+        public override void Kill(int timeLeft) {
+            Utils.PoofOfSmoke(projectile.Center);
+            NPC.NewNPC((int) projectile.Center.X, (int) projectile.Center.Y, mod.NPCType<NPCs.Enemy.Boss.ShadeStalker>());
+        }
+    }
+}
