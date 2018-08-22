06e69b9c598f6eeccaa4c7a17caf2b6fff9ce10f
SolarEclipse28 <areon_blade@yahoo.com>
2018-08-22 14:34:00 -0700

    Added Biome Cannons.

---
+++ Items/Ranged/DivineCannon.cs
@@ -0,0 +1,122 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace DrakSolz.Items.Ranged
+{
+	public class DivineCannon : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+            DisplayName.SetDefault("Divine Cannon");
+			Tooltip.SetDefault("???");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 2000;
+			item.ranged = true;
+			item.width = 40;
+			item.height = 20;
+			item.useTime = 40;
+			item.useAnimation = 40;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 3;
+            item.value = Item.buyPrice(1, 0, 0, 0);
+			item.rare = 5;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = false;
+			item.shoot = 116;
+			item.shootSpeed = 15f;
+			item.useAmmo = AmmoID.Rocket;
+		}
+
+		/*public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "ExampleItem", 10);
+			recipe.AddTile(null, "ExampleWorkbench");
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}*/
+
+		// What if I wanted this gun to have a 38% chance not to consume ammo?
+		/*public override bool ConsumeAmmo(Player player)
+		{
+			return Main.rand.NextFloat() >= .38f;
+		}*/
+
+		// What if I wanted it to work like Uzi, replacing regular bullets with High Velocity Bullets?
+		// Uzi/Molten Fury style: Replace normal Bullets with Highvelocity
+		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (type == ProjectileID.Bullet) // or ProjectileID.WoodenArrowFriendly
+			{
+				type = ProjectileID.BulletHighVelocity; // or ProjectileID.FireArrow;
+			}
+			return true; // return true to allow tmodloader to call Projectile.NewProjectile as normal
+		}*/
+
+		// What if I wanted it to shoot like a shotgun?
+		// Shotgun style: Multiple Projectiles, Random spread 
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+            int pro = Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+            Main.projectile[pro].penetrate = 10;
+            return true;
+        }
+
+		// What if I wanted an inaccurate gun? (Chain Gun)
+		// Inaccurate Gun style: Single Projectile, Random spread 
+		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+			speedX = perturbedSpeed.X;
+			speedY = perturbedSpeed.Y;
+			return true;
+		}
+
+		// What if I wanted multiple projectiles in a even spread? (Vampire Knives) 
+		// Even Arc style: Multiple Projectile, Even Spread 
+		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			float numberProjectiles = 3 + Main.rand.Next(3); // 3, 4, or 5 shots
+			float rotation = MathHelper.ToRadians(45);
+			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}*/
+
+		// Help, my gun isn't being held at the handle! Adjust these 2 numbers until it looks right.
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-2, 0);
+		}
+
+		// How can I make the shots appear out of the muzzle exactly?
+		// Also, when I do this, how do I prevent shooting through tiles?
+		/*public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 25f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			return true;
+		}*/
+		        public class AridCannonGlobalNPC : GlobalNPC {
+            public override void NPCLoot(NPC npc) {
+                if (npc.type == mod.NPCType<NPCs.Enemy.Endgame.Secret.DivineSister>()) {
+                    Item.NewItem((int) npc.position.X, (int) npc.position.Y, npc.width, npc.height, mod.ItemType<Items.Ranged.DivineCannon>(), 1);
+                }
+            }
+        }
+    }
+}
