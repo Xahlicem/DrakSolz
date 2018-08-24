5926ba76ddbf27726d116cf43c99e88dead76dcf
SolarEclipse28 <areon_blade@yahoo.com>
2018-08-24 16:30:41 -0700

    Added more NPCs

--- NPCs/Enemy/Endgame/Corrupt/CorruptMod.cs
+++ NPCs/Enemy/Endgame/Corrupt/CorruptMod.cs
@@ -8,12 +8,32 @@ namespace DrakSolz.NPCs.Enemy.Endgame.Corrupt {
     public class CorruptMod : GlobalNPC {
 
         public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo) {
-            if (spawnInfo.player.ZoneCorrupt && NPC.downedMoonlord) {
+            if (spawnInfo.player.ZoneCorrupt && spawnInfo.player.ZoneOverworldHeight && NPC.downedMoonlord) {
                 pool.Clear();
+                pool.Add(mod.NPCType<CarrionCollector>(), 40f);
+                pool.Add(mod.NPCType<SoulWraith>(), 25f);
+                pool.Add(mod.NPCType<VorpalReaver>(), 15f);
                 pool.Add(mod.NPCType<NPCs.Enemy.Endgame.Secret.DemonicSister>(), 1f);
             }
-            if (spawnInfo.player.ZoneCrimson && NPC.downedMoonlord) {
+            if (spawnInfo.player.ZoneCorrupt && spawnInfo.player.ZoneRockLayerHeight && NPC.downedMoonlord) {
                 pool.Clear();
+                pool.Add(mod.NPCType<CarrionCollector>(), 40f);
+                pool.Add(mod.NPCType<SoulWraith>(), 25f);
+                pool.Add(mod.NPCType<VorpalReaver>(), 15f);
+                pool.Add(mod.NPCType<NPCs.Enemy.Endgame.Secret.DemonicSister>(), 1f);
+            }
+            if (spawnInfo.player.ZoneCrimson && spawnInfo.player.ZoneOverworldHeight && NPC.downedMoonlord) {
+                pool.Clear();
+                pool.Add(mod.NPCType<CarrionCollector>(), 40f);
+                pool.Add(mod.NPCType<SoulWraith>(), 25f);
+                pool.Add(mod.NPCType<VorpalReaver>(), 15f);
+                pool.Add(mod.NPCType<NPCs.Enemy.Endgame.Secret.DemonicSister>(), 1f);
+            }
+            if (spawnInfo.player.ZoneCrimson && spawnInfo.player.ZoneRockLayerHeight && NPC.downedMoonlord) {
+                pool.Clear();
+                pool.Add(mod.NPCType<CarrionCollector>(), 40f);
+                pool.Add(mod.NPCType<SoulWraith>(), 25f);
+                pool.Add(mod.NPCType<VorpalReaver>(), 15f);
                 pool.Add(mod.NPCType<NPCs.Enemy.Endgame.Secret.DemonicSister>(), 1f);
             }
         }
---
+++ NPCs/Enemy/Endgame/Corrupt/SoulWraith.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DrakSolz.NPCs.Enemy.Endgame.Corrupt {
+    public class SoulWraith : ModNPC {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Soul Wraith");
+            Main.npcFrameCount[npc.type] = 4;
+        }
+
+        public override void SetDefaults() {
+            npc.CloneDefaults(NPCID.Wraith);
+            npc.scale = 1;
+            npc.width = 60;
+            npc.height = 60;
+            //npc.aiStyle = 39;
+            aiType = NPCID.Wraith;
+            animationType = NPCID.Wraith;
+            npc.damage = 140;
+            npc.defense = 1000;
+            npc.lifeMax = 60000;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 20f;
+            npc.knockBackResist = 0.05f;
+            banner = npc.type;
+            bannerItem = mod.ItemType<Items.Banners.LittleMushroomBanner>();
+        }
+        public override void AI() {
+            npc.TargetClosest(true);
+            Vector2 enemy = npc.Center;
+            enemy.Y = Main.player[npc.target].Center.Y;
+            float y = npc.velocity.Y;
+            if (npc.HasValidTarget && Main.player[npc.target].Distance(enemy) > 20f) {
+                npc.velocity = new Vector2(npc.direction * 1.8f, y);
+            }
+        }
+    }
+}
