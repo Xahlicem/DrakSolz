2fa60dfd71324fe7137bab34a22a9dda47799ed6
SolarEclipse28 <areon_blade@yahoo.com>
2019-01-02 09:12:02 -0800

    Fixed Projectile creation on Lizard Buff, now doesn't stop weapons from shooting projectiles.

--- Buffs/CrystalLizardBuff.cs
+++ Buffs/CrystalLizardBuff.cs
@@ -28,7 +28,7 @@ namespace DrakSolz.Buffs {
 			bool petProjectileNotSpawned = player.ownedProjectileCounts[mod.ProjectileType("CrystalLizardPet")] <= 0;
 			if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
 			{
-				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("ExamplePet"), 0, 0f, player.whoAmI, 0f, 0f);
+				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("CrystalLizardPet"), 0, 0f, player.whoAmI, 0f, 0f);
 			}
 		}
 	}
