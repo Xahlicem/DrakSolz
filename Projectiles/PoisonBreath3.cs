1e63a696ac7834f2a403e63d5d886e1382004690
SolarEclipse28 <areon_blade@yahoo.com>
2019-02-04 16:17:11 -0800

    Added increase fire damage.

--- Projectiles/PoisonBreath3.cs
+++ Projectiles/PoisonBreath3.cs
@@ -25,14 +25,17 @@ namespace DrakSolz.Projectiles {
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
             target.AddBuff(BuffID.Ichor, 600);
+            target.AddBuff(BuffID.Oiled, 600);
         }
 
         public override void OnHitPlayer(Player target, int damage, bool crit) {
             target.AddBuff(BuffID.Ichor, 600);
+            target.AddBuff(BuffID.Oiled, 600);
         }
 
         public override void OnHitPvp(Player target, int damage, bool crit) {
             target.AddBuff(BuffID.Ichor, 600);
+            target.AddBuff(BuffID.Oiled, 600);
         }
         public override void AI() {
 
