e217a32b94b1a7c5d2cfd0068eae9e49093b0094
SolarEclipse28 <areon_blade@yahoo.com>
2019-02-05 16:49:55 -0800

    Added weaker and stronger varient for Magnet Sphere as un-craftable loot.

---
+++ Items/Magic/MagnetSphere/ElectricSphere.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace DrakSolz.Items.Magic.MagnetSphere {
+    public class ElectricSphere : ModItem {
+        public override string Texture { get { return "Terraria/Item_1266"; } }
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Electric Sphere");
+            Tooltip.SetDefault("Summons an electric sphere to attack enemies.");
+        }
+
+        public override void SetDefaults() {
+            item.CloneDefaults(ItemID.MagnetSphere);
+            item.useStyle = 5;
+            item.damage = 20;
+            item.useTime = 60;
+            item.useAnimation = 60;
+            item.mana = 10;
+            item.knockBack = 4f;
+            item.shootSpeed = 10.0f;
+            item.value = Item.buyPrice(0, 2, 0, 0);
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+            int pro = Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+            Main.projectile[pro].frame = 1;
+            Main.projectile[pro].scale *= 0.5f;
+            return false;
+        }
+                public class ElectricSphereGlobalNPC : GlobalNPC {
+            public override void NPCLoot(NPC npc) {
+                if (Main.rand.Next(30) == 0) {
+                    if (npc.type == NPCID.GoblinSorcerer) {
+                        Item.NewItem((int) npc.position.X, (int) npc.position.Y, npc.width, npc.height, mod.ItemType<Items.Magic.MagnetSphere.ElectricSphere>(), 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Items/Magic/MagnetSphere/FulminatingSphere.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace DrakSolz.Items.Magic.MagnetSphere {
+    public class FulminatingSphere : ModItem {
+        public override string Texture { get { return "Terraria/Item_1266"; } }
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Fulminating Sphere");
+            Tooltip.SetDefault("Summons a fulminating sphere to attack enemies.");
+        }
+
+        public override void SetDefaults() {
+            item.CloneDefaults(ItemID.MagnetSphere);
+            item.useStyle = 5;
+            item.damage = 100;
+            item.mana = 20;
+            item.knockBack = 7f;
+            item.shootSpeed = 0.2f;
+            item.value = Item.buyPrice(0, 80, 0, 0);
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+            int pro = Projectile.NewProjectile(Main.mouseX + Main.screenPosition.X, Main.mouseY + Main.screenPosition.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+            Main.projectile[pro].frame = 1;
+            Main.projectile[pro].scale *= 1.5f;
+            return false;
+        }
+        public class FulminatingSphereGlobalNPC : GlobalNPC {
+            public override void NPCLoot(NPC npc) {
+                if (Main.rand.Next(5) == 0 && NPC.downedAncientCultist) {
+                    if (npc.type == NPCID.Eyezor) {
+                        Item.NewItem((int) npc.position.X, (int) npc.position.Y, npc.width, npc.height, mod.ItemType<Items.Magic.MagnetSphere.FulminatingSphere>(), 1);
+                    }
+                }
+            }
+        }
+    }
+}
